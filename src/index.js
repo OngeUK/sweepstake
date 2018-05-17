11444efb8a15d88629ce350378da4ced8078ec7e
Onge <onge.uk@gmail.com>
2018-05-17 22:44:46 +0100

    Add preact devtools and stylelint fixes

--- src/index.js
+++ src/index.js
@@ -2,20 +2,22 @@ import {Component, render, h} from "preact";
 import styled from "styled-components";
 
 const Title = styled.h1`
-	text-align: center;
 	font-family: sans-serif;
+	position: absolute;
+	text-align: center;
 `;
 
 class App extends Component {
 	render() {
 		return (
 			<div>
-				<Title>Example...</Title>
+				<Title>Example</Title>
 			</div>
 		);
 	}
 }
 
 if (typeof window !== "undefined") {
-	render(<App />, document.getElementById("root"));
+	render(<App />, document.body, document.body.lastElementChild);
+	require("preact/devtools");
 }
